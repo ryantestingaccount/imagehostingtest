adf2e1e309ee5e4acaadd8dbdb491569b8e23649
ryantestingaccount <crubbled52@hotmail.com>
2026-01-28 21:17:23 -0500

    Create gallery.js

---
+++ gallery.js
@@ -0,0 +1,33 @@
+const cloudName = "YOUR_CLOUD_NAME";
+const apiKey = "YOUR_READ_ONLY_API_KEY";
+
+const url = `https://api.cloudinary.com/v1_1/${cloudName}/resources/image?max_results=30`;
+
+fetch(url, {
+  headers: {
+    Authorization: "Basic " + btoa(apiKey + ":")
+  }
+})
+  .then(res => res.json())
+  .then(data => {
+    const gallery = document.getElementById("gallery");
+
+    data.resources.forEach(img => {
+      const card = document.createElement("div");
+      card.className = "card";
+
+      const image = document.createElement("img");
+      image.src = img.secure_url;
+
+      const downloadLink = document.createElement("a");
+      downloadLink.href =
+        `download.html?img=${encodeURIComponent(img.secure_url)}`;
+      downloadLink.textContent = "Download";
+      downloadLink.target = "_blank";
+
+      card.appendChild(image);
+      card.appendChild(downloadLink);
+      gallery.appendChild(card);
+    });
+  })
+  .catch(err => console.error(err));
